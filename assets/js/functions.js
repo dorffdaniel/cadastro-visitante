83870070ed7bafa0dea50d9060df1498fe0b7fdf
Dorffdaniel <danielcontabill@hotmail.com>
2026-01-19 20:53:48 +0000

    feat: adicionado nodejs e integracao com mysql

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,275 +1,60 @@
-let visitante = []
+const url = ' http://127.0.0.1:3002';
 
-
-let estado = ['RH', 'TI', 'Financeiro', 'Comercial'];
+let estado = ['Masculino', 'Feminino', 'Outro', 'Prefiro nao responder'];
 
 function showEstado() {
-    let msg = `<option value='-1'> campo vazio </option>`
+    let msg = `<option value='-1'> Selecione um sexo </option>`
 
     estado.forEach(element => {
         msg += `<option value=${element}>${element}</option>`
     })
 
-    $('#setor').html(msg);
-
+    $('#sexo').html(msg);
 }
 
-showEstado();
-
-
-
-
-let link = window.location.pathname;
-
-
-if (link == '/javascript/visitante/index.html') {
-    if (localStorage.getItem('chaveVisitante') != null) {
-
-        visitante = pegarVisitante();
-
-    }
-}
-
-
-let chaveCadastro = '18248118';
-let dadosGuardados = [];
-
-if (link == '/javascript/visitante/tabela.html') {
-    if (localStorage.getItem('chaveVisitante') != null) {
-
-
-        visitante = pegarVisitante();
-
-        mostrarTabela()
-
-
-    }
-}
-
-
-
-function cadastrarVisitante() {
+async function cadastrarVisitante() {
 
     let nome = $('#nome').val();
     let email = $('#email').val();
     let senha = $('#senha').val();
-    let cpfEncrip = $('#cpf').val();
+    let cpf = $('#cpf').val();
     let data = $('#data').val();
-    let setor = $('#setor').val();
-    let coment = $('#coment').val();
+    let sexo = $('#sexo').val();
 
-
-    const encriptando = CryptoJS.AES.encrypt(JSON.stringify(cpfEncrip), chaveCadastro).toString();
-
-    if (veri(email)) {
-
-        visitante.push(new Visitantes(nome, email, senha, encriptando, data, setor, coment))
-
-        alerta("cadastrado com sucesso", "Cadastrado Visitante", "success");
-
-        guardarVisitante()
-
-    } else {
-        alerta(`ja se econtra cadastrado`, "Erro", "error");
+    if (!nome.trim() || !email.trim() || !senha.trim() || !cpf.trim() || !data || sexo == '-1') {
+        alerta("Erro", "Preencha todos os campos", "error");
+        return;
     }
 
-}
+    const path = url + '/cadastrar';
+    const dados = { nome, email, senha, cpf, data, sexo };
+
+    try {
+
+        const conteduo = await fetch(path, {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify(dados)
+        });
+
+        let resp = await conteduo.json();
+
+        if (resp.erro) {
+            alerta("Erro", resp.erro, "error");
+        }
 
 
-//DEPOIS DE CADASTRAR TENHO QUE MANDAR PARA O SESSION E USO DESTA FORMA 
-function guardarVisitante() {
-    localStorage.setItem('chaveVisitante', JSON.stringify(visitante));
+        if (resp.msg) {
+            alerta("Cadastrado", resp.msg, "success");
+            $("#nome, #email, #senha, #cpf, #data").val('');
+            $("#sexo").val("-1");
+        }
 
-}
 
-function pegarVisitante() {
-    const dadosVis = JSON.parse(localStorage.getItem('chaveVisitante'))
-
-    let verifVisi = [];
-
-    for (let i = 0; i < dadosVis.length; i++) {
-        let visiNovos = Object.assign(new Visitantes(), dadosVis[i])
-
-        verifVisi.push(visiNovos)
+    } catch (error) {
+        console.log(error);
     }
 
-    return verifVisi;
-}
-
-
-
-
-function mostrarTabela() {
-
-    if ($.fn.DataTable.isDataTable('#dtExample')) {
-        $('#dtExample').DataTable().destroy();
-    }
-
-    let txt = "";
-
-    visitante.forEach((element, index) => {
-
-        /*    let cpfCriptografado = CryptoJS.AES.encrypt(JSON.stringify(element.cpf), chaveCadastro).toString(); */
-
-        let cpfCriptografado = element.cpf;
-
-        txt += `<tr>`
-        txt += `<td> ${element.nome}</td>`
-        txt += `<td> ${cpfCriptografado}</td>`
-        txt += `<td> ${element.data}</td>`
-        txt += `<td> <button type='button' class='btn btn-info' onclick='inform(${index})'>Info</button> </td>`
-        txt += `<td> <button type='button' class='btn btn-info' onclick='cpf(${index})'>ver</button> </td>`
-        txt += `<td> <button type='button' class='btn btn-info' onclick='delete1(${index})'>delete</button> </td>`
-        txt += `</tr>`
-    })
-
-    $('#listagem').html(txt);
-
-    //faz parte do database
-    $('#dtExample').DataTable({
-        "pageLength": 25
-    });
-
-}
-
-
-function inform(valor) {
-    $('#modalId').modal('show');
-
-    let guard = visitante[valor]
-
-    let txt = ''
-
-    txt += `<ul>`
-    txt += `<li> nome: ${guard.nome}</li>`
-    txt += `<li> data: ${guard.data}</li>`
-    txt += `<li> setor: ${guard.setor}</li>`
-    txt += `<li> comentario: ${guard.coment}</li>`
-    txt += `</ul>`
-
-    $('#resModal').html(txt);
-
-}
-
-
-function cpf() {
-    $('#modalCpf').modal('show');
-
-}
-
-
-function verificarSenha() {
-
-    let confSenha = $('#confirmSenha').val();
-
-    let encontrou = false;
-
-    let msg = ``
-    visitante.forEach(elem => {
-        if(confSenha == elem.senha) {
-            encontrou = true
-            msg += `<ul>`
-            msg += `<li> cpf: ${elem.mostrarCpf(chaveCadastro)}</li>`
-            msg += `</ul>`
-
-        }
-      
-    });
-
-    if(!encontrou){
-        msg = 'senha incorreta'
-    }
-    $('#confirmSenha').val('')
-     $('#resMod').html(msg); 
-
-}
-
-
-function closeMod(){
-    $('#resMod').html(' '); 
-    $('#confirmSenha').val('')
-}
-
-
-function delete1(valor) {
-
-    visitante.splice(valor, 1)
-
-    guardarVisitante()
-    mostrarTabela()
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function veri(valor) {
-    let flag = true;
-
-    visitante.forEach(elemento => {
-        if (valor == elemento.email) {
-            flag = false
-        }
-    })
-
-    return flag;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function getSelect(arr, flag) {
-
-    let txt = `<option value= '-1'> campo vazio </option>`
-
-    arr.forEach((element, index) => {
-
-        if (flag == 1) {
-            txt += `<option value = "${element.nome}"> ${element.nome}</option>`
-        }
-
-        if (flag == 2) {
-            txt += `<option value ="${index}"> ${element.nomeFunc}</option>`
-        }
-        if (flag == 3) {
-            txt += `<option value = "${index}"> ${element.filme}</option>`
-        }
-
-    })
-    return txt;
 }
 
 
@@ -301,17 +86,5 @@ function alerta(msg, titulo, icon) {
 
 
 $(document).ready(function () {
-    $('select').select2();
+    showEstado();
 });
-
-
-
-$(document).ready(function () {
-
-    $('#idModal').on('shown.bs.modal', function () {
-        $("#idDoSelect").select2({
-            dropdownParent: $('#idDoModal')
-        });
-    });
-
-})
---
+++ server/app.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import conn from '../server/bd.js';
+import cors from 'cors';
+import crypt from 'bcrypt';
+
+let app = express();
+let porta = 3002;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+
+    res.end('home do servidor');
+
+})
+
+app.post('/cadastrar', async (req, res) => {
+
+    const { nome, email, senha, cpf, data, sexo } = req.body;
+
+    if (!nome || !email || !senha || !cpf) {
+        res.status(400).json({ erro: 'Nome , email, senha e cpf sao obrigratorios' })
+    }
+
+    if (senha.length < 5) {
+        res.status(400).json({ erro: 'senha muito curta' });
+    }
+
+    try {
+
+        const saltRounds = 10;
+        let hash = await crypt.hash(senha, saltRounds);
+
+        let sql = "INSERT INTO visitante(nome, email, senha, cpf, dataNasc, sexo) values (?, ?, ?, ?, ?, ?)";
+
+        conn.query(sql, [nome, email, hash, cpf, data, sexo], (erro, result) => {
+            if (erro) {
+                res.status(500).json({ erro: 'erro ao cadastrar visitante' });
+            } else {
+                res.status(200).json({ msg: 'cadastrado com sucesso' });
+            }
+
+        });
+
+    } catch (error) {
+        res.status(500).json({ erro: 'falha ao gerar hash da senha' });
+    }
+
+})
+
+
+app.listen(porta, () => {
+    console.log(`servidor rodando: http://localhost:${porta}/`)
+})
